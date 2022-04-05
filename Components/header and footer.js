b9d35b037dfd01561529a3a1a946fccee34b9a7c
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2022-04-05 00:04:50 -0700

    Portfolio updates

--- Components/header and footer.js
+++ Components/header and footer.js
@@ -17,6 +17,12 @@ class MyHeader extends HTMLElement {
                     <li class="nav-item">
                       <a class="nav-link" href="/about.html">About Me</a>
                     </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Contact Me</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href=".Images/Venelin-Mitev-Portfolio.pdf" download>Download my Resume</a>
+                    </li>
                     <li class="nav-item dropdown">
                       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Coding Projects
@@ -52,16 +58,16 @@ class MyFooter extends HTMLElement {
           <div class="container-fluid">
             <div class="row">
                 <div class="col-sm">
-                    <a href="https://github.com/VeniMitev" target="_blank"><p class="github">_github <i class="bi bi-github"></i></p></a>
+                    <a href="https://github.com/VeniMitev" target="_blank"><p class="github">github<span class="blink_me">_</span> <i class="bi bi-github"></i></p></a>
                 </div>
                 <div class="col-sm">
-                    <a href="https://www.linkedin.com/in/venelin-mitev/" target="_blank"><p class="linkedin">_linkedin <i class="bi bi-linkedin"></i></p></a>
+                    <a href="https://www.linkedin.com/in/venelin-mitev/" target="_blank"><p class="linkedin">linkedin<span class="blink_me">_</span> <i class="bi bi-linkedin"></i></p></a>
                 </div>
                 <div class="col-sm">
-                    <a href="https://www.youtube.com/channel/UCoGzGTZdwwVaActFVhqygZg" target="_blank"><p class="youtube">_youtube <i class="bi bi-youtube"></i></p></a>
+                    <a href="https://www.youtube.com/channel/UCoGzGTZdwwVaActFVhqygZg" target="_blank"><p class="youtube">youtube<span class="blink_me">_</span> <i class="bi bi-youtube"></i></p></a>
                 </div>
                 <div class="col-sm">
-                    <a href="https://www.instagram.com/venelin_m" target="_blank"><p class="instagram">_instagram <i class="bi bi-instagram"></i></p></a>
+                    <a href="https://www.instagram.com/venelin_m" target="_blank"><p class="instagram">instagram<span class="blink_me">_</span> <i class="bi bi-instagram"></i></p></a>
                 </div>
             </div>
             <div class="row">
@@ -77,3 +83,29 @@ class MyFooter extends HTMLElement {
 }
 
 customElements.define('my-footer', MyFooter);
+
+
+
+    // <footer>
+    //       <div class="container-fluid">
+    //         <div class="row">
+    //             <div class="col-sm">
+    //                 <a href="https://github.com/VeniMitev" target="_blank"><p class="github">github<div class="blink_me">_</div> <i class="bi bi-github"></i></p></a>
+    //             </div>
+    //             <div class="col-sm">
+    //                 <a href="https://www.linkedin.com/in/venelin-mitev/" target="_blank"><p class="linkedin">linkedin<div class="blink_me">_</div> <i class="bi bi-linkedin"></i></p></a>
+    //             </div>
+    //             <div class="col-sm">
+    //                 <a href="https://www.youtube.com/channel/UCoGzGTZdwwVaActFVhqygZg" target="_blank"><p class="youtube">youtube<div class="blink_me">_</div> <i class="bi bi-youtube"></i></p></a>
+    //             </div>
+    //             <div class="col-sm">
+    //                 <a href="https://www.instagram.com/venelin_m" target="_blank"><p class="instagram">instagram<div class="blink_me">_</div> <i class="bi bi-instagram"></i></p></a>
+    //             </div>
+    //         </div>
+    //         <div class="row">
+    //             <div class="col-sm">
+    //                 <p style="font-size: 0.75em; font-family: monospace">&copy; <span id="year"></span> by Veni Mitev</p>
+    //             </div>
+    //         </div>
+    //       </div>
+    //     </footer>
