9ce5bd5e8c053d46714b22c891becc73ce469f9b
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2022-04-14 23:02:04 -0700

    Updates to website

--- Components/header and footer.js
+++ Components/header and footer.js
@@ -12,7 +12,7 @@ class MyHeader extends HTMLElement {
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                      <a class="nav-link" aria-current="page" href="/">Home</a>
+                      <a class="nav-link" aria-current="page" href="/#">Home</a>
                     </li>
                     <li class="nav-item">
                       <a class="nav-link" href="/#about-me">About Me</a>
