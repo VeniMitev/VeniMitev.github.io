29f1d690723a77176a5306af903fb2c7a000b179
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2023-03-23 00:48:50 -0700

    Updating packages. Working on Footer

--- next-portfolio/app/layout.tsx
+++ next-portfolio/app/layout.tsx
@@ -2,6 +2,11 @@ import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
 import './globals.css';
 
+export const metadata = {
+    title: 'Veni Mitev | Web Developer',
+    description: 'Full-Stack developer | PRoduct Manager | Photographer | Traveler',
+}
+
 export default function RootLayout({
     children,
 }: {
--- next-portfolio/components/Footer.tsx
+++ next-portfolio/components/Footer.tsx
@@ -10,19 +10,20 @@ import {
 
 const Footer = () => {
     return (
-        <footer className='mt-auto flex h-max w-screen justify-center overflow-hidden align-middle md:h-24'>
-            <div className=' w-max transform overflow-hidden rounded-md border bg-gray-50 p-2 text-center drop-shadow-sm transition duration-700 ease-in-out hover:translate-y-0 hover:drop-shadow-xl md:w-3/6 md:translate-y-10'>
-                <code>&copy; Veni Mitev - {new Date().getFullYear()}</code>
+        <footer className='flex justify-center w-screen mt-auto overflow-hidden align-middle h-max md:h-28'>
+            <div className='p-2 overflow-hidden text-center transition duration-500 ease-in-out transform border rounded-lg bg-gray-50 w-max hover:bg-white drop-shadow-sm hover:translate-y-2 hover:drop-shadow-xl md:w-3/6 md:translate-y-14'>
+                <code>&copy; Veni Mitev {new Date().getFullYear()}</code>
                 <br />
 
-                <div className='m-2 flex flex-col flex-wrap md:flex-row'>
+                <div className='flex flex-col flex-wrap m-2 md:flex-row'>
                     <Link
                         className='m-1 inline-flex grow justify-center align-middle hover:text-[#171515]'
                         href='https://github.com/VeniMitev'
                         target='_blank'
                     >
-                        <FaGithub className='m-1 self-center' />
+                        <FaGithub className='self-center m-1' />
                         GitHub
+                        <span className='font-extrabold animate-pulse'>_</span>
                     </Link>
 
                     <Link
@@ -30,33 +31,58 @@ const Footer = () => {
                         href='https://www.linkedin.com/in/venelin-mitev/'
                         target='_blank'
                     >
-                        <FaLinkedin className='m-1 self-center' />
+                        <FaLinkedin className='self-center m-1' />
                         LinkedIn
+                        <span className='font-extrabold animate-pulse'>_</span>
                     </Link>
                     <Link
                         className='m-1 inline-flex grow justify-center align-middle hover:text-[#8a3ab9]'
                         href='https://www.instagram.com/venelin_m'
                         target='_blank'
                     >
-                        <FaInstagram className='m-1 self-center' />
+                        <FaInstagram className='self-center m-1' />
                         Instagram
+                        <span className='font-extrabold animate-pulse'>_</span>
                     </Link>
                     <Link
                         className='m-1 inline-flex grow justify-center align-middle hover:text-[steelblue]'
                         href='mailto:business@venimitev.com'
                         target='_blank'
                     >
-                        <FaEnvelope className='m-1 self-center' />
+                        <FaEnvelope className='self-center m-1' />
                         Email
+                        <span className='font-extrabold animate-pulse'>_</span>
                     </Link>
                     <Link
                         className='m-1 inline-flex grow justify-center align-middle hover:text-[#ff0000]'
                         href='https://www.youtube.com/channel/UCoGzGTZdwwVaActFVhqygZg'
                         target='_blank'
                     >
-                        <FaYoutube className='m-1 self-center' />
+                        <FaYoutube className='self-center m-1' />
                         YouTube
+                        <span className='font-extrabold animate-pulse'>_</span>
                     </Link>
+                </div>
+                <div>
+                    <p className='text-xs'>
+                        Built with{' '}
+                        <Link
+                            className='text-blue-500 hover:text-blue-600'
+                            href='https://nextjs.org/'
+                            target='_blank'
+                        >
+                            Next.js
+                        </Link>{' '}
+                        and{' '}
+                        <Link
+                            className='text-blue-500 hover:text-blue-600'
+                            href='https://tailwindcss.com/'
+                            target='_blank'
+                        >
+                            Tailwind CSS
+                        </Link>
+                            
+                    </p>
                 </div>
             </div>
         </footer>
