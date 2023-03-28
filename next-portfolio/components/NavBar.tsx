ba3d5a3c48921583c7fbcb46e6633d10b95b4144
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2023-03-28 00:14:34 -0700

    Optimizing navbar for mobile

--- next-portfolio/components/NavBar.tsx
+++ next-portfolio/components/NavBar.tsx
@@ -11,22 +11,22 @@ const NavBar = ({
     mood: string;
 }) => {
     return (
-        <nav className='flex items-center justify-between px-4 py-3'>
+        <nav className='flex flex-col items-center px-4 py-3 md:flex-row md:justify-between'>
             <div className='flex justify-center text-center'>
-                <div>
+                <div className='m-0'>
                     <h1 className='text-2xl font-semibold tracking-tight text-gray-700 select-none md:text-3xl lg:text-4xl'>
                         VeniMitev.dev
                     </h1>
                 </div>
                 <button
-                    className='flex items-center px-3 py-2 mx-6 text-gray-700 border rounded hover:border-blue-500 hover:text-blue-500'
+                    className='flex items-center invisible w-0 h-0 p-0 text-gray-700 border rounded hover:border-blue-500 hover:text-blue-500 md:visible md:mx-6 md:h-auto md:w-auto md:py-2 md:px-3'
                     onClick={() => handleThemeChange()}
                 >
                     Switch to {mood === 'ocean' ? 'Forrest' : 'Ocean'}
                 </button>
             </div>
 
-            <div className='flex justify-center text-center'>
+            <div className='flex justify-around gap-5 text-center md:justify-center'>
                 <Link
                     href='/'
                     className='m-2 text-sm font-medium tracking-tight text-gray-700 lg:text-md hover:text-blue-500 lg:m-6'
@@ -51,12 +51,6 @@ const NavBar = ({
                 >
                     Contact
                 </Link>
-                {/* <Link
-                    href='/'
-                    className='m-2 text-sm font-medium tracking-tight text-gray-700 lg:text-md hover:text-blue-500 lg:m-6'
-                >
-                    Download CV
-                </Link> */}
             </div>
         </nav>
     );
