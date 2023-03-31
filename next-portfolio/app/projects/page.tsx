73a33be22cbe97fbe1a8a4362fbea0f553d1e894
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2023-03-30 22:58:21 -0700

    Starting work on projects page

--- next-portfolio/app/projects/page.tsx
+++ next-portfolio/app/projects/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const Projects = () => {
-  return (
-    <div className='flex items-center justify-center'>
-        <h1 className='text-3xl font-semibold'>Projects Coming Soon...</h1>
-    </div>
-  )
-}
+    return (
+        <>
+            <h1 className='text-center py-6'>
+                <span className='text-3xl font-semibold'>Projects</span>
+            </h1>
+            <div className='mx-auto grid max-w-5xl grid-cols-3 items-center justify-center gap-4'>
+                <Project />
+                <Project />
+                <Project />
+            </div>
+        </>
+    );
+};
 
-export default Projects
+const Project = () => {
+    return (
+        <div className='max-w-xl justify-self-center bg-white'>
+            <h1 className='px-8'>Project</h1>
+        </div>
+    );
+};
+
+export default Projects;
