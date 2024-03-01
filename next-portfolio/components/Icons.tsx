e13ec0215b023c7c388ef9059c133108724cd6cb
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2024-03-01 01:14:10 -0800

    Updating Icons

--- next-portfolio/components/Icons.tsx
+++ next-portfolio/components/Icons.tsx
@@ -10,14 +10,14 @@ const Icons = ({ technologies }: { technologies: Technology[] }) => {
                 <h2 className='my-4 select-none text-center text-lg font-medium'>
                     Technologies:
                 </h2>
-                <div className='grid grid-cols-2 content-center gap-3 md:grid-cols-3 lg:grid-cols-4'>
+                <div className='grid grid-cols-3 content-center gap-3 md:grid-cols-4 lg:grid-cols-5'>
                     {technologies
                         .filter((item) => !item.comingSoon && item.active)
                         .sort((a, b) => a.sort - b.sort)
                         .map((item) => (
                             <div
                                 key={item.title}
-                                className='mx-1 inline-flex transform select-none !items-center gap-3 p-2 drop-shadow-sm transition duration-700 hover:scale-105 hover:animate-pulse hover:drop-shadow-lg '
+                                className='mx-1 flex flex-col transform select-none !items-center gap-3 p-2 drop-shadow-sm transition duration-700 hover:scale-105 hover:animate-pulse hover:drop-shadow-lg '
                             >
                                 <Image
                                     draggable={false}
@@ -29,6 +29,7 @@ const Icons = ({ technologies }: { technologies: Technology[] }) => {
                                     placeholder='blur'
                                     blurDataURL={item.icon.image}
                                 />
+
                                 <span className='font-sans text-lg font-semibold'>
                                     {item.title}
                                 </span>
@@ -48,7 +49,7 @@ const Icons = ({ technologies }: { technologies: Technology[] }) => {
                         .map((item) => (
                             <div
                                 key={item.title}
-                                className='mx-1 inline-flex transform select-none !items-center gap-3 p-2 drop-shadow-sm transition duration-700 hover:scale-105 hover:animate-pulse hover:drop-shadow-lg '
+                                className='mx-1 flex flex-col transform select-none !items-center gap-3 p-2 drop-shadow-sm transition duration-700 hover:scale-105 hover:animate-pulse hover:drop-shadow-lg '
                             >
                                 <Image
                                     draggable={false}
