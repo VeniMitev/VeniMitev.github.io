c88cdfff27f9435eb592c4496ebf0508ad48ae84
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2024-06-11 01:23:00 -0700

    Update layout to improve UX for largest screens by limiting content at 1920px

--- next-portfolio/tailwind.config.js
+++ next-portfolio/tailwind.config.js
@@ -14,6 +14,12 @@ module.exports = {
         youtube: "#ff0000",
       }
     },
+    screens: {
+      xs: "475px",
+      '3xl': "1600px",
+      '4xl': "1920px",
+      ...require("tailwindcss/defaultConfig").theme.screens
+    }
   },
   plugins: [],
   darkMode: "class",
