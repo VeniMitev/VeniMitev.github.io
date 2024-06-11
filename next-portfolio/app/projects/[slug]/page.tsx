a3c130eac9c1b18ae0e3f6f3fbaf57fa65cfb519
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2024-06-11 00:02:17 -0700

    Reverting changes to attempt to fix crashing at build

--- next-portfolio/app/projects/[slug]/page.tsx
+++ next-portfolio/app/projects/[slug]/page.tsx
@@ -4,37 +4,39 @@ import { getSingleProject } from '../../../sanity/sanity-utils';
 import { PortableText } from '@portabletext/react';
 
 // TODO: Implement page for every project for case studies, info, etc.
-
 const ProjectPage = async ({ params }: { params: { slug: string } }) => {
     const project = await getSingleProject(params.slug);
 
     return (
         <section className='flex flex-col items-center justify-center'>
-            <BackToProjects />
-        
+            <Link
+                className='ml-4 self-start text-xs font-bold uppercase text-blue-400 hover:text-blue-600 lg:ml-10 lg:text-base'
+                href='/projects'
+            >
+                {'<- Back to Projects'}
+            </Link>
+
             <h1 className='my-6 text-center text-xl font-semibold'>
                 <span className='text-3xl font-semibold'>Case Study:</span>
                 <br /> {project?.title}
             </h1>
 
-            {project?.image && (
-                <Image
-                    src={project.image}
-                    alt={project.title}
-                    width={1000}
-                    height={600}
-                    className='mb-5 w-fit rounded-sm p-1 shadow-lg lg:max-w-screen-md'
-                    placeholder='blur'
-                    blurDataURL={project.image}
-                />
-            )}
+            <Image
+                src={project.image}
+                alt={project.title}
+                width={1000}
+                height={600}
+                className='mb-5 w-fit rounded-sm p-1 shadow-lg lg:max-w-screen-md'
+                placeholder='blur'
+                blurDataURL={project.image}
+            />
 
             <section className='m-4 mx-2 my-10 transform rounded-md bg-gray-50 px-4 shadow-lg transition duration-500 ease-in-out hover:bg-white lg:mx-5 lg:px-6'>
                 <div className={'rich-text-container my-6 max-w-screen-md'}>
                     <PortableText value={project.caseStudy} />
                 </div>
             </section>
-
+            
             <section>
                 <h2 className='my-6 text-center text-xl font-semibold'>
                     Technology Stack Used:
@@ -59,15 +61,5 @@ const ProjectPage = async ({ params }: { params: { slug: string } }) => {
         </section>
     );
 };
-
-const BackToProjects = () => {
-    return (
-        <Link href='/projects'>
-            <a className='ml-4 self-start text-xs font-bold uppercase text-blue-400 hover:text-blue-600 lg:ml-10 lg:text-base'>
-                {'<- Back to Projects'}
-            </a>
-        </Link>
-    );
-}
 
 export default ProjectPage;
