f1b3ec303850c1100c50b471cafd45214826fd0a
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2024-07-30 23:25:15 -0700

    Update CV download link and add new file

--- next-portfolio/components/DownloadCV.tsx
+++ next-portfolio/components/DownloadCV.tsx
@@ -5,8 +5,8 @@ const DownloadCV = () => {
     return (
         <button className='bg-blue-300 flex items-center rounded p-2 hover:border-blue-500 hover:text-blue-500'>
             <Link
-                href='/Venelin_Mitev_Resume.pdf'
-                download={'Venelin_Mitev_Resume.pdf'}
+                href='/Veni_Mitev_Resume.pdf'
+                download={'Veni_Mitev_Resume.pdf'}
                 target='_blank'
                 className='lg:text-md text-sm font-medium tracking-tight text-gray-700 hover:text-white'
             >
