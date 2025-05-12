2dd45494a77194b95a3caa3462fb9f86d13f4f43
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2025-05-11 19:17:40 -0700

    Fix DownloadCV component to correctly reference resume file and filename for download

--- next-portfolio/components/DownloadCV.tsx
+++ next-portfolio/components/DownloadCV.tsx
@@ -6,16 +6,14 @@ const DownloadCV = async () => {
     const data = await getHomePage();
     const resume = data.resume;
 
-    const { file, fileNameOnDownload } = resume;
-
-    if (!file) {
+    if (!resume) {
         return null; // or some fallback UI
     }
 
     return (
         <button className='bg-blue-300 flex items-center rounded p-2 hover:border-blue-500 hover:text-blue-500'>
             <Link
-                href={`${file}?dl=${fileNameOnDownload}.pdf`}
+                href={`${resume.file}?dl=${resume.fileNameOnDownload}.pdf`}
                 target='_blank'
                 rel='noreferrer'
                 download
