da40ee8abe0733d7cbafe28f240b580242071e59
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2025-05-13 20:59:27 -0700

    refactor: remove commented-out podcast section from Home component

--- next-portfolio/app/(frontend)/page.tsx
+++ next-portfolio/app/(frontend)/page.tsx
@@ -62,24 +62,6 @@ export default async function Home() {
                     blurDataURL={data.certification.image}
                 />
             </div>
-
-            {/* <section className='mx-10 my-2 '>
-                <h2>
-                    <span className='text-center text-2xl font-semibold'>
-                        Learn more about me
-                    </span>
-                </h2>
-                <iframe
-                    title='Unpopular Passion Podcast - Veni Mitev'
-                    loading='lazy'
-                    className='mx-auto my-6 w-full select-none'
-                    id='podcast'
-                    src='https://open.spotify.com/embed/episode/2A4VXiol7U1mRBaNE2QLMV?utm_source=generator'
-                    width='100%'
-                    height='300em'
-                    allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
-                />
-            </section> */}
         </div>
     );
 }
